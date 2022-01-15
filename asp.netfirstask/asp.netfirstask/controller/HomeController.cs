7ff0ca31789a25668a1f21f2a7870c1ac7a0b2e5
ChimnazM <88970139+ChimnazM@users.noreply.github.com>
2022-01-15 14:44:52 +0400

    Add files via upload

---
+++ asp.netfirstask/asp.netfirstask/controller/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netfirstask.controller
+{
+    public class HomeController : Controller
+    {
+        public ViewResult index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "index";
+            return view;
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
